04bf2ab2ed0533ee5cd1849bfd98d8b98752c49f
David Robert <contact.davidrobert@gmail.com>
2022-06-28 15:25:12 +0200

    first page

---
+++ src/Controller/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// je créé une classe avec le même nom que le fichier
+// suffixée par controller
+class PageController
+{
+
+    /**
+     * @Route("/")
+     *
+     * je créé une route, en utilisant des commentaires PHP
+     * et "@Route" pour spécifier l'url de la page que je veux créer
+     * La route est située au dessus de la méthode à appeler
+     * pour la page
+     */
+    public function home()
+    {
+        // je renvoie une réponse HTTP grâce à l'objet Response
+        // (du composant http foundation de Symfony)
+        // Ca affichera donc 'hello accueil' quand
+        // l'url "/" sera demandée
+        return new Response('Hello Accueil');
+    }
+
+}
